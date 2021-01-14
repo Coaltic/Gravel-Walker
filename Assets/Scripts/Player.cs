f30159bbccd7a76cf2f7fd228a953358b0863a85
Coaltic <glindsay156@gmail.com>
2021-01-14 15:16:46 -0400

    added killbox

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TRIGGERED: " + other.tag);
+
+        if (other.tag == "Killbox")
+        {
+            Debug.Log("About to reset player");
+            //gameObject.transform.position = originalPosition;
+            gameObject.transform.position = new Vector3(0, 1, 0);
+        }
+    }
+}
