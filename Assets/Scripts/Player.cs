d9e94dfdf936151f1f06407457027cd0a8195f5e
Coaltic <glindsay156@gmail.com>
2021-01-19 09:50:29 -0400

    Checkpoints

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Vector3 originalPosition;
+    public GameObject canvas;
+    public float timer = 3.0f;
 
     void Start()
     {
@@ -23,8 +25,39 @@ public class Player : MonoBehaviour
         if (other.tag == "Killbox")
         {
             Debug.Log("About to reset player");
-            //gameObject.transform.position = originalPosition;
-            gameObject.transform.position = new Vector3(0, 1, 0);
+            gameObject.transform.position = originalPosition;
+            //gameObject.transform.position = new Vector3(0, 1, 0);
         }
+        else if (other.tag == "Checkpoint1")
+        {
+            originalPosition = gameObject.transform.position;
+            Debug.Log("Checkpoint 1 reached");
+            canvas.SetActive(true);
+            gameObject.transform.position = originalPosition;   
+
+        }
+        else if (other.tag == "Checkpoint2")
+        {
+            originalPosition = gameObject.transform.position;
+            Debug.Log("Checkpoint 2 reached");
+            canvas.SetActive(true);
+            gameObject.transform.position = originalPosition;
+
+
+        }
+        else if (other.tag == "Checkpoint3")
+        {
+            originalPosition = gameObject.transform.position;
+            Debug.Log("Checkpoint 3 reached");
+            canvas.SetActive(true);
+            gameObject.transform.position = originalPosition;
+
+        }
+        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        canvas.SetActive(false);
     }
 }
