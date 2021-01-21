a006c80581818d65353b1f618d62ca57a3aa766d
Coaltic <glindsay156@gmail.com>
2021-01-21 15:18:57 -0400

    Teleportation Added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{    
+    public GameObject respawnLocation;
+    Vector3 winner = new Vector3(-1.8f, 7.0f, -75.0f);
     Vector3 originalPosition;
     public GameObject canvas;
+
     public float timer = 3.0f;
 
     void Start()
     {
-        originalPosition = gameObject.transform.position;
+        
     }
 
     void Update()
@@ -20,20 +23,26 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Vector3 respawnLocation =  originalPosition - gameObject.transform.position;
         Debug.Log("TRIGGERED: " + other.tag);
 
         if (other.tag == "Killbox")
         {
             Debug.Log("About to reset player");
-            gameObject.transform.position = originalPosition;
+            gameObject.GetComponent<CharacterController>().Move(respawnLocation);
             //gameObject.transform.position = new Vector3(0, 1, 0);
+        }
+        else if (other.tag == "Checkpoint")
+        {
+            originalPosition = gameObject.transform.position;
+            gameObject.transform.position = originalPosition;
         }
         else if (other.tag == "Checkpoint1")
         {
             originalPosition = gameObject.transform.position;
             Debug.Log("Checkpoint 1 reached");
             canvas.SetActive(true);
-            gameObject.transform.position = originalPosition;   
+            gameObject.transform.position = originalPosition;
 
         }
         else if (other.tag == "Checkpoint2")
@@ -52,6 +61,11 @@ public class Player : MonoBehaviour
             canvas.SetActive(true);
             gameObject.transform.position = originalPosition;
 
+        }
+        else if (other.tag == "Teleporter")
+        {
+            Debug.Log("TRIGGERED: " + other.tag);
+            gameObject.GetComponent<CharacterController>().Move(winner);
         }
         
     }
